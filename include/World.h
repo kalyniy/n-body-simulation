c9841bcdbafc93a3f425e98db3792d5d89a3eeee
Dan K <bohdan.k2000@gmail.com>
2025-10-19 22:18:56 -0400

    increased world size

--- include/World.h
+++ include/World.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define WORLD_WIDTH 600
-#define WORLD_HEIGHT 600
-#define WORLD_DEPTH 600
+#define WORLD_WIDTH 2000 // 600
+#define WORLD_HEIGHT 2000 // 600
+#define WORLD_DEPTH 2000 // 600
